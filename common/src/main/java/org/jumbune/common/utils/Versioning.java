3477d067856350d102fe286be8e903c60352916c
BhandariArpan <arpan3189@gmail.com>
2014-07-10 16:49:47 +0530

    Changed build versioning to 1.1.1-SNAPSHOT

--- common/src/main/java/org/jumbune/common/utils/Versioning.java
+++ common/src/main/java/org/jumbune/common/utils/Versioning.java
@@ -14,9 +14,9 @@ public final class Versioning {
 	}
 	
 	/** The Constant BUILD_VERSION. */
-	public static final String BUILD_VERSION="1.1.0";
+	public static final String BUILD_VERSION="1.1.1";
 	
 	/** The Constant DISTRIBUTION_NAME. */
-	public static final String DISTRIBUTION_NAME="";
+	public static final String DISTRIBUTION_NAME="-SNAPSHOT";
 
 }
